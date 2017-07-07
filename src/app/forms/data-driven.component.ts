57a645f4d66eff46c3ec315e58c88273fb77bf4c
aniruddha <aniruddha.kudalkar@gmail.com>
2017-07-07 09:10:07 +0530

    forms: data driven custom validations

--- src/app/forms/data-driven.component.ts
+++ src/app/forms/data-driven.component.ts
@@ -1,25 +1,36 @@
-import { Component} from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven',
   templateUrl: './data-driven.component.html',
   styleUrls: ['./data-driven.component.css']
 })
-export class DataDrivenComponent  {
-
+export class DataDrivenComponent {
 
   form: FormGroup;
+
+  cities = [
+    'Pune', 'Mumbai', 'Delhi', 'Nagpur'
+  ];
   constructor(private formBuilder: FormBuilder) {
     this.form = formBuilder.group({
-      fstNm: [''],
-      lstNm: [''],
-      email: [''],
+      fstNm: ['', Validators.compose([
+        Validators.required
+      ])],
+      lstNm: ['', Validators.compose([Validators.required, this.validateLstNm()])],
+      email: ['', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]],
       city: ['']
     });
   }
 
   onSubmit() {
     console.log(this.form);
+  }
+
+  validateLstNm(): ValidatorFn {
+    return (c: AbstractControl) => {
+      return c.value.charAt(0) !== 'a' ? { err: 'Word does not start from a' } : null;
+    };
   }
 }
